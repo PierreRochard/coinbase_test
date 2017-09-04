2e8e7cd4eb162782d8c048ca411dbd060de53e02
Pierre Rochard <pierre.rochard@axial.net>
2017-09-04 12:16:22 -0300

    adding comments

--- quote_service/queries/calculate_quote.py
+++ quote_service/queries/calculate_quote.py
@@ -9,11 +9,19 @@ from quote_service.models.orders import Orders
 
 def calculate_quote(pair_id: int, action: str, amount: Decimal,
                     is_inverted: bool):
+    """
+    :param pair_id: the ID of the currency pair
+    :param action: Buy or Sell
+    :param amount: base currency amount to be bought or sold
+    :param is_inverted: flag for when a quote is needed for reverse of pair_id's base and quote currencies
+    :return: The weighted average price at which the market order would be executed
+    """
     Orders.insert_orders(pair_id)
 
     orders_1 = alias(Orders)
     orders_2 = alias(Orders)
 
+    # Setup subquery params based on what side of the order book we are querying
     if (action == 'buy' and not is_inverted) or (action == 'sell' and is_inverted):
         side = 'ask'
         cumulative_filter = orders_1.c.price >= orders_2.c.price
@@ -27,6 +35,7 @@ def calculate_quote(pair_id: int, action: str, amount: Decimal,
     else:
         raise UnsupportedActionError()
 
+    # Subquery to calculate the cumulative size by price
     subquery = (
         db.session.query(orders_1.c.price.label('price'),
                          func.sum(orders_2.c.size * orders_2.c.price).label('inverse_size'),
@@ -38,8 +47,9 @@ def calculate_quote(pair_id: int, action: str, amount: Decimal,
             .group_by(orders_1.c.price)
             .subquery()
     )
+    # Check if the requested amount exceeds the Level 2 order book's capacity
+    # and set up params the price query
     _, max_inverted, max_ = db.session.query(subquery).order_by(subquery.c.size.desc()).first()
-
     if is_inverted:
         if amount > max_inverted:
             raise UnsupportedAmountError(max_inverted)
@@ -53,6 +63,9 @@ def calculate_quote(pair_id: int, action: str, amount: Decimal,
         size = Orders.size
         multiplier = Orders.price
 
+    # Query joins the cumulative price subquery and uses a case statement to
+    # determine how much of each order layer to consume
+    # Total consumed is then divided by the ordered amount to determine WA price
     price = (
         db.session.query(
             func.sum(
